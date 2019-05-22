36e9f108b1326c08fc460dc590688699229e982b
Raphael Nepomuceno <raphael.nepomuceno@protonmail.com>
2019-05-22 18:47:27 -0300

    Add Message to Request conversion method. (WIP)

--- lib/percussion/utils.ex
+++ lib/percussion/utils.ex
@@ -1,7 +1,11 @@
 defmodule Percussion.Utils do
   @moduledoc """
-  Utility functions that don't warrant their own modules.
+  Helpers which aren't particularly critical to the library and don't warrant their own
+  modules.
   """
+
+  alias Nostrum.Struct.Message
+  alias Percussion.Request
 
   @doc """
   Tries splitting the given `text` with `OptionParser.split/1`, and falls back to
@@ -21,5 +25,19 @@ defmodule Percussion.Utils do
     OptionParser.split(text)
   rescue
     _ in RuntimeError -> String.split(text)
+  end
+
+  @doc """
+  Creates a `t:Percussion.Request.t/0` from a `t:Nostrum.Struct.Message.t/0`.
+
+  Argument `contents` must be the unprefixed contents of the message that triggered
+  this command. Note that enforcing that `message` and `contents` matches is up to the
+  caller.
+  """
+  @spec to_request(Message.t(), String.t()) :: Request.t()
+  def to_request(%Message{} = message, contents) do
+    with [command | arguments] <- split(contents) do
+      %Request{arguments: arguments, invoked_with: command, message: message}
+    end
   end
 end
