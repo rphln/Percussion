1f924d9b5e7ff9d518eeba67e3c96ceab72f527a
Raphael Nepomuceno <raphael.nepomuceno@protonmail.com>
2019-06-07 20:54:07 -0300

    chore: Remove unused entries from .formatter.exs.

--- .formatter.exs
+++ .formatter.exs
@@ -1,15 +1,3 @@
-# Used by "mix format"
-locals_without_parens = [
-  command: 1,
-  command: 2,
-  match: 2,
-  match: 3,
-  redirect: 2,
-  redirect: 3
-]
-
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: locals_without_parens,
-  export: [locals_without_parens: locals_without_parens]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
 ]
