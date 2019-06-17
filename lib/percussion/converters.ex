40760335078edfa2e690847b72b3527e010d6de0
Raphael Nepomuceno <raphael.nepomuceno@protonmail.com>
2019-06-17 06:19:59 -0300

    Add `Converters.emoji_to_id/1`.

--- lib/percussion/converters.ex
+++ lib/percussion/converters.ex
@@ -8,6 +8,7 @@ defmodule Percussion.Converters do
   @channel_regex ~r/<#([0-9]+)>$/i
   @role_mention_regex ~r/<@&([0-9]+)>$/i
   @user_mention_regex ~r/<@!?([0-9]+)>$/i
+  @emoji_regex ~r/<:(?:\S+):([0-9]+)>$/i
 
   @doc """
   Converts a formatted Discord user mention to a `t:Nostrum.Snowflake.t/0`.
@@ -75,6 +76,30 @@ defmodule Percussion.Converters do
   @spec channel_to_id(String.t()) :: {:ok, Snowflake.t()} | :error
   def channel_to_id(text) when is_bitstring(text) do
     with [_text, match] <- Regex.run(@channel_regex, text) do
+      Snowflake.cast(match)
+    else
+      _ -> :error
+    end
+  end
+
+  @doc """
+  Converts a formatted Discord emoji to a `t:Nostrum.Snowflake.t/0`.
+
+  ## Examples
+
+      iex> Percussion.Converters.emoji_to_id("<:thonk:123456789>")
+      {:ok, 123456789}
+
+      iex> Percussion.Converters.emoji_to_id("<@123456789>")
+      :error
+
+      iex> Percussion.Converters.emoji_to_id("123456789")
+      :error
+
+  """
+  @spec emoji_to_id(String.t()) :: {:ok, Snowflake.t()} | :error
+  def emoji_to_id(text) when is_bitstring(text) do
+    with [_text, match] <- Regex.run(@emoji_regex, text) do
       Snowflake.cast(match)
     else
       _ -> :error
